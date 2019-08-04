06bb5f792f1e42aec1299cffbf15afa02aa1534a
Ball <piccomed@gmail.com>
2019-08-04 20:12:05 +0700

    Update Lottery.sol

--- smartcontracts/Lottery.sol
+++ smartcontracts/Lottery.sol
@@ -1,23 +1,6 @@
 pragma solidity ^0.4.20;
 
-contract Ownable {
-    address public owner;
 
-    function Ownable() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        owner = newOwner;
-    }
-
-}
 
 
 contract Lottery is Ownable {
